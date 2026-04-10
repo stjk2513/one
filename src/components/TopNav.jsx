45b090c096c569feb9b1ff84f1402d8885e44cfb
stjk2513 <stjk2513@gmail.com>
2026-04-10 02:20:52 -0400

    style topnav

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -2,13 +2,17 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
   border-bottom: 1px solid black;
+  padding: 10px;
 `;
 
 function TopNav() {
   return (
     <Nav>
-      <Link to="/">Home</Link> | <Link to="/counter">Counter</Link>
+      <Link to="/">Home</Link>
+      <Link to="/counter">Counter</Link>
     </Nav>
   );
 }
