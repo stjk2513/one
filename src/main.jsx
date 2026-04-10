9a94f0f584e5975a114e26c9bed0c3b76ba5ce82
stjk2513 <stjk2513@gmail.com>
2026-04-10 01:12:02 -0400

    connect the redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore } from "redux";
+import rootReducer from "./store/rootReducer";
 import "./index.css";
 import App from "./App.jsx";
 
+const store = createStore(rootReducer);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
